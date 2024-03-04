24fbc4dd0f6e24c2778d411f390adb12984195f9
Nelli <143870145+NelliVershinina@users.noreply.github.com>
2024-03-04 10:27:47 +0300

    Add files via upload

---
+++ FractalModule.pas
@@ -0,0 +1,81 @@
+﻿unit FractalModule;
+interface
+uses GraphABC;
+procedure DrawFractal(startX, startY, p: integer; scale: real);
+implementation
+const u = 10;
+
+procedure p1(dx, dy: integer; scale: real);
+begin
+  LineTo(PenX + Round(dx * scale), PenY + Round(dy * scale))
+end;
+
+procedure a(i: integer; scale: real); forward;
+procedure b(i: integer; scale: real); forward;
+procedure c(i: integer; scale: real); forward;
+procedure d(i: integer; scale: real); forward;
+
+procedure a(i: integer; scale: real);
+begin
+  if i > 0 then
+  begin
+    d(i - 1, scale);
+    p1(+u, 0, scale);
+    a(i - 1, scale);
+    p1(0, u, scale);
+    a(i - 1, scale);
+    p1(-u, 0, scale);
+    c(i - 1, scale);
+  end;
+end;
+
+procedure b(i: integer; scale: real);
+begin
+  if i > 0 then
+  begin
+    c(i - 1, scale);
+    p1(-u, 0, scale);
+    b(i - 1, scale);
+    p1(0, -u, scale);
+    b(i - 1, scale);
+    p1(u, 0, scale);
+    d(i - 1, scale);
+  end;
+end;
+
+procedure c(i: integer; scale: real);
+begin
+  if i > 0 then
+  begin
+    b(i - 1, scale);
+    p1(0, -u, scale);
+    c(i - 1, scale);
+    p1(-u, 0, scale);
+    c(i - 1, scale);
+    p1(0, u, scale);
+    a(i - 1, scale);
+  end;
+end;
+
+procedure d(i: integer; scale: real);
+begin
+  if i > 0 then
+  begin
+    a(i - 1, scale);
+    p1(0, u, scale);
+    d(i - 1, scale);
+    p1(u, 0, scale);
+    d(i - 1, scale);
+    p1(0, -u, scale);
+    b(i - 1, scale);
+  end;
+end;
+
+procedure DrawFractal(startX, startY, p: integer; scale: real);
+begin
+  ClearWindow;
+  MoveTo(startX, startY);
+  a(p, scale);
+end;
+
+end.
---
+++ основая программа.pas
@@ -0,0 +1,43 @@
+﻿program zd;
+uses GraphABC, FractalModule;
+var startX, startY, N: integer;
+scale: real;
+
+procedure KeyDown(Key: integer);
+begin
+  case Key of
+    VK_Left:  Dec(startX, 10 + 20);
+    VK_Right: Inc(startX, 10 + 20);
+    VK_Up:    Dec(startY, 10 + 20);
+    VK_Down:  Inc(startY, 10 + 20);
+  end;
+  DrawFractal(startX, startY, N, scale);
+end;
+
+procedure KeyUP(KeyR: integer);
+begin
+  case KeyR of
+    187, 107: // +
+      begin
+        scale := scale * 1.1;
+        DrawFractal(startX, startY, N, scale);
+      end;
+    189, 109: // -
+      begin
+        scale := scale / 1.1;
+        DrawFractal(startX, startY, N, scale);
+      end;
+  end;
+end;
+
+begin
+  writeln('Введите глубину рекурсии: ');
+  read(N);
+  startX := 100;
+  startY := 100;
+  scale := 1.0;
+  SetWindowSize(520, 520);
+  OnKeyDown := KeyDown;
+  OnKeyUp := KeyUP;
+  DrawFractal(startX, startY, N, scale);
+end.
